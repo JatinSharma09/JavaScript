b2aee58586bb9117ff99a362206a3aa755947d3e
SHARMA JATIN <133507224+JatinSharma6969@users.noreply.github.com>
2023-07-29 11:46:30 +0530

    Add files via upload

---
+++ 11_Functions.js
@@ -0,0 +1,33 @@
+let marks = {
+  harry: 90,
+  jatin: 75,
+  ajay: 82,
+  aditya: 78
+} 
+// Problem 1:
+    // for (let i = 0; i < Object.keys(marks).length; i++) {
+//    console.log("The marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]])
+// }
+// Problem 2:
+   // for (let key in marks)
+//   {
+//     console.log("The Marks Of " + key + " Is " +  marks[key]);
+//   }
+//
+
+// problem 3
+     // let cn = 4 ;
+     //  let i
+     //  while(i!=cn)
+     //    {
+     //     i= prompt("Enter The Number");
+     //      console.log("Try Again");
+     //    }      
+     //  console.log ("You Have Entered Correct Number");
+//  Problem 4
+
+    const mean =(a,b,c,d) =>
+      {
+        return (a+b+c+d)/4;
+      }
+      console.log(mean(1,2,3,4));
