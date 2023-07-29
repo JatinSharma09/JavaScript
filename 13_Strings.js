f17734f6f27a6012068b1a1e263f64d9a8b4e568
SHARMA JATIN <133507224+JatinSharma6969@users.noreply.github.com>
2023-07-29 11:48:35 +0530

    Add files via upload

---
+++ 13_Strings.js
@@ -0,0 +1,9 @@
+
+let name = "jatin"
+  let name2= "ajay"
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(1,4))
+// console.log(name.slice(2))
+// console.log(name.replace("ja","ra"))
+// console.log(name.concat(" Is A Best Freind of " + name2))
